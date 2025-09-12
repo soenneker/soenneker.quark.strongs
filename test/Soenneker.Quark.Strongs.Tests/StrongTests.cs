4828ac20c69d12fd9052774e6bd7773c6b342a07
Jake Soenneker <jake@soenneker.com>
2025-09-11 20:37:05 -0500

    Initial commit

---
+++ test/Soenneker.Quark.Strongs.Tests/StrongTests.cs
@@ -0,0 +1,18 @@
+﻿using Soenneker.Tests.FixturedUnit;
+using Xunit;
+
+namespace Soenneker.Quark.Strongs.Tests;
+
+[Collection("Collection")]
+public sealed class StrongTests : FixturedUnitTest
+{
+    public StrongTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    {
+    }
+
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
